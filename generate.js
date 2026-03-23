bd8577233d3118e44c0dd8086a457a3a98b5b6cf
TheIllusionist93 <felix.laicher+github@gmail.com>
2026-03-23 12:04:39 +0100

    Update generate.js

--- generate.js
+++ generate.js
@@ -9,7 +9,7 @@ registerFont('./Caveat-Regular.ttf', { family: 'Caveat' });
 // ═══════════════════════════════════════════════════════════════════
 
 const PROJECT_CONFIG = {
-  totalDays: 76,
+  totalDays: 77,
   startDate: '2025-11-17',  // Format: YYYY-MM-DD
   projectName: 'DOCTOR',
   
